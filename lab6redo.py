5e5ad263d67301bf02426abe29595ad2ff1fa40a
kiml11 <127216556+kiml11@users.noreply.github.com>
2023-03-22 22:32:02 -0400

    Add files via upload

---
+++ lab6redo.py
@@ -0,0 +1,33 @@
+def print_menu():
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+# creates and prints the menu
+
+
+def encoder(password):
+    encoded_password = " "  # created an empty string for a list
+    for x in password:
+        temp_pass = int(x)
+        temp_pass = int(temp_pass + 3) % 10
+        encoded_password = encoded_password + str(temp_pass)
+    return encoded_password
+
+
+if __name__ == '__main__':
+    # prints menu and asks for input in a loop
+    while True:
+        # function that prints menu
+        print_menu()
+        option = int(input("\nPlease enter an option: "))   # asks user for option input
+        if option == 1:
+            password = input("Please enter your password to encode: ")
+            print("Your password has been encoded and stored!\n")
+            print(encoder(password))
+        elif option == 2:
+            password = input("Please enter your password to decode: ")
+            print("The encoded password is, and the original password is \n")
+        elif option == 3:
+            break
